6ea477cd14f51671408077a443591806352176b3
Alessio De Vito <alessio.devito@jetbrains.com>
2023-09-12 08:02:14 +0200

    Refactor SpriteFontXmlSchemaProvider for optimization
    Simplified handling of cached schema in the `SpriteFontXmlSchemaProvider` class. The extraneous schemas map was removed in favor of having a single cached schema instance. This change reduces complexity while maintaining performance, as the schema is only calculated once and reused as needed.

--- src/rider/main/kotlin/me/seclerp/rider/plugins/monogame/spritefont/SpriteFontXmlSchemaProvider.kt
+++ src/rider/main/kotlin/me/seclerp/rider/plugins/monogame/spritefont/SpriteFontXmlSchemaProvider.kt
@@ -2,7 +2,6 @@ package me.seclerp.rider.plugins.monogame.spritefont
 
 import com.intellij.javaee.ExternalResourceManagerEx
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.ModificationTracker
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.psi.PsiFile
@@ -12,46 +11,39 @@ import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
 import com.intellij.psi.xml.XmlFile
 import com.intellij.xml.XmlSchemaProvider
-import com.jetbrains.rd.platform.util.getLogger
-import com.jetbrains.rd.platform.util.getOrCreateUserData
+import com.intellij.openapi.diagnostic.logger
 
 class SpriteFontXmlSchemaProvider : XmlSchemaProvider() {
     companion object {
-        private val logger = getLogger<SpriteFontXmlSchemaProvider>()
+        private val logger = logger<SpriteFontXmlSchemaProvider>()
         const val SCHEMA_LOCATION = "/xmlSchema/SpriteFont.xsd"
         const val FILE_EXTENSION = "spritefont"
     }
 
-    private val schemaFileMapKey: Key<MutableMap<String, CachedValue<XmlFile>>> = Key.create(
-        "RIDER_SCHEMAS_FILE_MAP_KEY:" + javaClass.name)
-    private var schemas: MutableMap<String, CachedValue<XmlFile>>? = null
+    private var schema: CachedValue<XmlFile>? = null
 
     override fun getSchema(url: String, module: Module?, baseFile: PsiFile): XmlFile? {
-        logger.info("map $schemaFileMapKey")
-        val location = SCHEMA_LOCATION
+        logger.info("Schema location: $SCHEMA_LOCATION")
         val project = baseFile.project
         logger.info("Project: ${project.name}")
 
-        schemas = schemas ?: project.getOrCreateUserData(schemaFileMapKey) { mutableMapOf() }
-
-        val cachedValue = schemas?.get(location)
-        if (cachedValue != null) {
-            logger.info("Returning cached schema for location: $location")
-            return cachedValue.value
+        if (schema != null) {
+            logger.info("Returning cached schema for location: $SCHEMA_LOCATION")
+            return schema?.value
         }
 
-        val schema = CachedValuesManager.getManager(project).createCachedValue(object : CachedValueProvider<XmlFile> {
+        schema = CachedValuesManager.getManager(project).createCachedValue(object : CachedValueProvider<XmlFile> {
             override fun compute(): CachedValueProvider.Result<XmlFile> {
                 logger.info("Computing value for cached schema")
-                val resource = javaClass.getResource(location)!!
+                val resource = javaClass.getResource(SCHEMA_LOCATION)!!
                 val fileByURL = VfsUtil.findFileByURL(resource)
                 if (fileByURL == null) {
-                    logger.error("xsd file '$location' not found")
+                    logger.error("xsd file '$SCHEMA_LOCATION' not found")
                     return CachedValueProvider.Result(null, ModificationTracker.EVER_CHANGED)
                 }
 
                 val psiFile = PsiManager.getInstance(project).findFile(fileByURL)
-                if (psiFile !is XmlFile){
+                if (psiFile !is XmlFile) {
                     logger.warn("PSI file is not XML file")
                     return CachedValueProvider.Result(null, ModificationTracker.EVER_CHANGED)
                 }
@@ -63,9 +55,8 @@ class SpriteFontXmlSchemaProvider : XmlSchemaProvider() {
             }
         }, false)
 
-        logger.info("Added schema to cache for location: $location")
-        schemas?.put(location, schema)
-        return schema.value
+        logger.info("Added schema to cache for location: $SCHEMA_LOCATION")
+        return schema?.value
     }
 
     override fun isAvailable(file: XmlFile): Boolean {
