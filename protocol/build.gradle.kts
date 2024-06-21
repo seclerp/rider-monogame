911fffd5e692645209f754380af7db0f619be1b6
Andrii Rublov <andrii.rublov@jetbrains.com>
2024-06-21 19:17:13 +0200

    [monogame]: Migrate to IJ Platform Plugin 2.0, also migrate RD

--- protocol/build.gradle.kts
+++ protocol/build.gradle.kts
@@ -1,19 +1,63 @@
+import com.jetbrains.rd.generator.gradle.RdGenTask
+
 plugins {
-    id("java")
+    id("com.jetbrains.rdgen")
     id("org.jetbrains.kotlin.jvm")
 }
 
-val rdLibDirectory: () -> File by rootProject.extra
-
 repositories {
-    maven { setUrl("https://cache-redirector.jetbrains.com/maven-central") }
-    flatDir {
-        dir(rdLibDirectory())
+    maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+    maven("https://cache-redirector.jetbrains.com/maven-central")
+}
+
+val repoRoot: File = projectDir.parentFile
+
+sourceSets {
+    main {
+        kotlin {
+            srcDir(repoRoot.resolve("protocol/src/main/kotlin/model"))
+        }
     }
 }
 
+rdgen {
+    verbose = true
+    packages = "model"
+
+    generator {
+        language = "kotlin"
+        transform = "asis"
+        root = "com.jetbrains.rider.model.nova.ide.IdeRoot"
+        namespace = "com.jetbrains.rider.plugins.efcore.model"
+        directory = file("$projectDir/src/rider/main/kotlin/me/seclerp/rider/plugins/monogame/rd").absolutePath
+    }
+
+    generator {
+        language = "csharp"
+        transform = "reversed"
+        root = "com.jetbrains.rider.model.nova.ide.IdeRoot"
+        namespace = "Rider.Plugins.EfCore"
+        directory = file("$projectDir/src/dotnet/Rider.Plugins.MonoGame/Rd").absolutePath
+    }
+}
+
+tasks.withType<RdGenTask> {
+    dependsOn(sourceSets["main"].runtimeClasspath)
+    classpath(sourceSets["main"].runtimeClasspath)
+}
+
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation(group = "", name = "rd-gen")
-    implementation(group = "", name = "rider-model")
+    val rdVersion: String by project
+    val rdKotlinVersion: String by project
+
+    implementation("com.jetbrains.rd:rd-gen:$rdVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$rdKotlinVersion")
+    implementation(
+        project(
+            mapOf(
+                "path" to ":",
+                "configuration" to "riderModel"
+            )
+        )
+    )
 }
