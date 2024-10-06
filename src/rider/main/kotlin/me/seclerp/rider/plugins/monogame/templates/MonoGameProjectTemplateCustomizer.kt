f1a562617ca435c573efb1e07bd83af3e6591c55
Andrii Rublov <seclerp.dev@gmail.com>
2024-10-06 22:29:32 +0200

    Templates: handle non-installed case with instructions

--- src/rider/main/kotlin/me/seclerp/rider/plugins/monogame/templates/MonoGameProjectTemplateCustomizer.kt
+++ src/rider/main/kotlin/me/seclerp/rider/plugins/monogame/templates/MonoGameProjectTemplateCustomizer.kt
@@ -2,7 +2,7 @@ package me.seclerp.rider.plugins.monogame.templates
 
 import com.jetbrains.rider.projectView.projectTemplates.providers.ProjectTemplateCustomizer
 
-class MonoGameProjectTemplateCustomizer : ProjectTemplateCustomizer {
+internal class MonoGameProjectTemplateCustomizer : ProjectTemplateCustomizer {
     private val monoGameTemplateTypes = setOf(MonoGameTemplateType())
     override fun getCustomProjectTemplateTypes() = monoGameTemplateTypes
 }
---
+++ src/rider/main/kotlin/me/seclerp/rider/plugins/monogame/templates/MonoGameTemplateMetadata.kt
@@ -0,0 +1,13 @@
+package me.seclerp.rider.plugins.monogame.templates
+
+internal object MonoGameTemplateMetadata {
+    internal object Names {
+        const val CROSS_PLATFORM_APP = "Cross-Platform Desktop App"
+        const val WINDOWS_DESKTOP_APP = "Windows Desktop App"
+        const val ANDROID_APP = "Android App"
+        const val IOS_APP = "iOS App"
+        const val GAME_LIB = "Game Library"
+        const val CONTENT_PIPELINE_EXTENSION = "Content Pipeline Extension"
+        const val SHARED_LIB = "Shared Library Project"
+    }
+}
